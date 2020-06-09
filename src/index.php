8db59d4236ce5cca8e210f3176b40318507b424e
root <root@ip-172-31-34-176.ec2.internal>
2020-06-09 16:59:47 +0000

    Added files for project

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Тестируем PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Test, php!</p>';
+       echo 'Line2';
+#exec("sudo hostname " . $hostname);
+# host=$(hostname);
+#   echo "$hostname";
+?>
+ </body>
+</html>
